ed4aa1d9280422ca1b5fe0d358ff34b97e838a8a
vinzenz messerli <vmesserli@gmail.com>
2015-12-14 00:39:25 +0100

    Consume Documents with filter

---
+++ PoCeHealthLive/Model/DocumentReference.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PoCeHealthLive.Model
+{
+    class DocumentReference
+    {
+        public string UUID { get; set; }
+        public DocumentReference(string uuid)
+        {
+            this.UUID = uuid;
+        }
+    }
+}
--- PoCeHealthLive/ViewModel/EpdDocumentViewModel.cs
+++ PoCeHealthLive/ViewModel/EpdDocumentViewModel.cs
@@ -1,4 +1,15 @@
-﻿using PoCeHealthLive.Model;
+﻿using java.net;
+using org.ehealth_connector.cda.enums;
+using org.ehealth_connector.common;
+using org.ehealth_connector.communication;
+using org.ehealth_connector.communication.ch.enums;
+using org.ehealth_connector.communication.xd.storedquery;
+using org.ehealth_connector.security;
+using org.ehealth_connector.security.xua;
+using org.openhealthtools.ihe.atna.nodeauth;
+using org.openhealthtools.ihe.atna.nodeauth.context;
+using org.openhealthtools.ihe.xds.response;
+using PoCeHealthLive.Model;
 using PoCeHealthLive.ViewModel.Commands;
 using System;
 using System.Collections.Generic;
@@ -18,6 +29,9 @@ namespace PoCeHealthLive.ViewModel
         public ObservableCollection<DocumentAttributes> ConfidentialityCodes { get; set; }
         public ObservableCollection<DocumentAttributes> PracticeSettingCodes { get; set; }
         public ObservableCollection<DocumentAttributes> FormatCodes { get; set; }
+        public ObservableCollection<DocumentReference> RecievedDocumentReferences { get; set; }
+
+        private XUAConfig config = XUAConfig.getInstance();
 
         // Property backing fields.
         string selectedClassCode;
@@ -101,6 +115,7 @@ namespace PoCeHealthLive.ViewModel
             this.ConfidentialityCodes = new ObservableCollection<DocumentAttributes>();
             this.PracticeSettingCodes = new ObservableCollection<DocumentAttributes>();
             this.FormatCodes = new ObservableCollection<DocumentAttributes>();
+            this.RecievedDocumentReferences = new ObservableCollection<DocumentReference>();
 
             // Set Document attributes
             this.SetDocumentAttributes();
@@ -108,8 +123,9 @@ namespace PoCeHealthLive.ViewModel
         }
 
         public void SetDocumentAttributes()
-        {    
+        {
             // Set Document Class Codes
+            this.ClassCodes.Add(new DocumentAttributes("Alle", null));
             this.ClassCodes.Add(new DocumentAttributes("Konsultation Notizen", "DTC01"));
             this.ClassCodes.Add(new DocumentAttributes("Verlauf Notizen", "DTC02"));
             this.ClassCodes.Add(new DocumentAttributes("Behandlungen Notizen", "DTC03"));
@@ -126,6 +142,7 @@ namespace PoCeHealthLive.ViewModel
             this.ClassCodes.Add(new DocumentAttributes("Andere", "DTC90"));
 
             // Set Document Healthcarefacility Codes
+            this.HealthCareFacilityCodes.Add(new DocumentAttributes("Alle", null));
             this.HealthCareFacilityCodes.Add(new DocumentAttributes("Institut für medizinische Diagnostik", "190001"));
             this.HealthCareFacilityCodes.Add(new DocumentAttributes("Notfalleinrichtung/Rettungswesen", "190002"));
             this.HealthCareFacilityCodes.Add(new DocumentAttributes("Gesundheitsbehörde", "190003"));
@@ -141,6 +158,7 @@ namespace PoCeHealthLive.ViewModel
             this.HealthCareFacilityCodes.Add(new DocumentAttributes("Andere", "190999"));
 
             // Set Document Confidentiality Codes
+            this.ConfidentialityCodes.Add(new DocumentAttributes("Alle", null));
             this.ConfidentialityCodes.Add(new DocumentAttributes("Administrative", "A"));
             this.ConfidentialityCodes.Add(new DocumentAttributes("Medical", "N"));
             this.ConfidentialityCodes.Add(new DocumentAttributes("Secret", "V"));
@@ -148,6 +166,7 @@ namespace PoCeHealthLive.ViewModel
             this.ConfidentialityCodes.Add(new DocumentAttributes("Utilities", "U"));
 
             // Set Document PracticeSetting Codes
+            this.PracticeSettingCodes.Add(new DocumentAttributes("Alle", null));
             this.PracticeSettingCodes.Add(new DocumentAttributes("Allergologie und klinische Immunologie", "260001"));
             this.PracticeSettingCodes.Add(new DocumentAttributes("Allgemeinmedizin", "260002"));
             this.PracticeSettingCodes.Add(new DocumentAttributes("Anästhesiologie", "260003"));
@@ -216,9 +235,10 @@ namespace PoCeHealthLive.ViewModel
             this.PracticeSettingCodes.Add(new DocumentAttributes("Zahnheilkunde", "260066"));
             this.PracticeSettingCodes.Add(new DocumentAttributes("Zahn-, Mund- und Kieferheilkunde", "260067"));
             this.PracticeSettingCodes.Add(new DocumentAttributes("Hämatologie", "260068"));
-            this.PracticeSettingCodes.Add(new DocumentAttributes("Andere medzini-sche Fachrichtung", "260099"));
+            this.PracticeSettingCodes.Add(new DocumentAttributes("Andere medzinische Fachrichtung", "260099"));
 
-            // Set Document Format Codes    
+            // Set Document Format Codes
+            this.FormatCodes.Add(new DocumentAttributes("Alle", null));
             this.FormatCodes.Add(new DocumentAttributes("application/pdf", "urn:ihe:iti:xds-sd:pdf:2008"));
             this.FormatCodes.Add(new DocumentAttributes("text/plain", "urn:ihe:iti:xds-sd:text:2008"));
             this.FormatCodes.Add(new DocumentAttributes("mage/jpeg", "urn:ihe:iti-fr:xds-sd:jpeg:2010"));
@@ -229,6 +249,208 @@ namespace PoCeHealthLive.ViewModel
         public void SearchDocumentsInRegistry()
         {
             Debug.WriteLine("Methode SearchDocumentInRegistry ausgeführt");
+            //SetSearchAttributes();
+            ConvenienceCommunication conCom = getInfomedCommunication();
+            java.util.List documentEntries;
+            XDSQueryResponseType qr;
+
+            initConfig();
+
+            qr = conCom.queryDocumentsReferencesOnly(SetSearchAttributes());
+
+            // Get  document IDs of references 
+            documentEntries = qr.getReferences();
+            string sKeyWordDocumentIdStart = "urn:uuid";
+            string sKeyWordDocumentIdEnd = ")";
+            String[] docUUIDs = new String[60];
+
+            for (int i = 0; i < documentEntries.size(); ++i)
+            {
+                string sourceString = documentEntries.get(i).ToString();
+                int keyWordDocumentIdBegin = sourceString.IndexOf(sKeyWordDocumentIdStart);
+                string DocumentId = sourceString.Substring(keyWordDocumentIdBegin);
+                int ikeyWordDocumentIdEnd = DocumentId.IndexOf(sKeyWordDocumentIdEnd);
+                DocumentId = DocumentId.Substring(0, ikeyWordDocumentIdEnd);
+                //RecievedDocumentReferences.Add(new DocumentReference(DocumentId));
+                docUUIDs[i] = DocumentId;
+                System.Console.WriteLine(DocumentId);
+            }
+
+            ShowQueryDocumentsReferencesResult(docUUIDs);
+        }
+
+        private void ShowQueryDocumentsReferencesResult(String[] docUUIDs)
+        {
+
+            RecievedDocumentReferences.Clear();
+            for (int i = 0; i < docUUIDs.Length; i++)
+            {
+                RecievedDocumentReferences.Add(new DocumentReference(docUUIDs[i]));
+            }
+        }
+
+        public FindDocumentsQuery SetSearchAttributes()
+        {
+            // Identificator patientId = new Identificator("2.16.756.5.30.1.120.10.1", "214211038"); Messerli Vinzenz
+            Identificator patientId = new Identificator("2.16.756.5.30.1.120.10.1", "102836133"); // ersetzen
+            AdministrativeGender sex = AdministrativeGender.MALE;
+
+            // Set classCode common DTC06: Episode Zusamenfassungen
+            //new Code("2.16.756.5.30.1.120.20.1", "DTC01", "2.16.756.5.30.1.120.20.1^DTC01")
+            Code[] classCodes = new Code[1];
+            if (!string.IsNullOrEmpty(SelectedClassCode))
+            {
+                classCodes[0] =  new Code("2.16.756.5.30.1.120.20.1", SelectedClassCode, 
+                    "2.16.756.5.30.1.120.20.1^" + SelectedClassCode);
+            }
+            else { classCodes = null; }
+
+            //if(classCodes == null || classCodes.Length == 0 )
+
+            // Set DateTimeRange Attribute
+            DateTimeRangeAttributes name = DateTimeRangeAttributes.CREATION_TIME;
+            java.util.Date from = new java.util.Date(2015, 12, 11);
+            java.util.Date to = new java.util.Date(2015, 12, 11);
+            DateTimeRange[] dateTimeRange = new DateTimeRange[4];
+            dateTimeRange[0] = new DateTimeRange(name, (java.util.Date)from, to);
+
+            // Set healthCareFacilityCodes
+            //Code[] healthCareFacilityCodes = { new Code("2.16.756.5.30.1.127.3.2.1.19",
+            //    "190010", "2.16.756.5.30.1.127.3.2.1.19^190010") };
+            Code[] healthCareFacilityCodes = new Code[1];
+            if (!string.IsNullOrEmpty(SelectedHealthCareFacilityCode))
+            {
+                healthCareFacilityCodes[0] = new Code("2.16.756.5.30.1.127.3.2.1.19", SelectedHealthCareFacilityCode,
+                    "2.16.756.5.30.1.127.3.2.1.19^" + SelectedHealthCareFacilityCode);
+            }
+            else { healthCareFacilityCodes = null; }
+
+            // Set confidentialityCode "N" = normal, "R" = restricted, "V" = very restricted
+            //Code[] confidentialityCodes = { new Code("2.16.756.5.30.1.120.20.3", "N",
+            //    "2.16.756.5.30.1.120.20.3^N") };
+            Code[] confidentialityCodes = new Code[1];
+            if (!string.IsNullOrEmpty(SelectedConfidentialityCode))
+            {
+                confidentialityCodes[0] = new Code("2.16.756.5.30.1.120.20.3", SelectedConfidentialityCode, 
+                    "2.16.756.5.30.1.120.20.3^" + SelectedConfidentialityCode);
+            }
+            else { confidentialityCodes = null; }
+
+            // Set practiceSettingCode 260003: Anästhesiologie
+            //Code[] practiceSettingCodes = { new Code("2.16.756.5.30.1.127.3.2.1.26",
+            //    "260059", "2.16.756.5.30.1.127.3.2.1.26^260059") };
+            Code[] practiceSettingCodes = new Code[1];
+            if (!string.IsNullOrEmpty(SelectedPracticeSettingCode))
+            {
+                practiceSettingCodes[0] = new Code("2.16.756.5.30.1.127.3.2.1.26", SelectedPracticeSettingCode, 
+                    "2.16.756.5.30.1.127.3.2.1.26^" + SelectedPracticeSettingCode);
+            }
+            else { practiceSettingCodes = null; }
+
+            // Set formatCode urn:ihe:iti:xds-sd:pdf:2008 = application/pdf
+            //Code[] formatCodes = { new Code("1.3.6.1.4.1.19376.1.2.3", "urn:ihe:iti:xds-sd:pdf:2008",
+            //        "1.3.6.1.4.1.19376.1.2.3^urn:ihe:iti:xds-sd:pdf:2008") };
+            Code[] formatCodes = new Code[1];
+            if (!string.IsNullOrEmpty(SelectedFormatCode))
+            {
+                formatCodes[0] = new Code("1.3.6.1.4.1.19376.1.2.3", SelectedFormatCode,
+                    "1.3.6.1.4.1.19376.1.2.3^" + SelectedFormatCode);
+            }
+            else { formatCodes = null; }
+
+                FindDocumentsQuery fdq = new FindDocumentsQuery(patientId, classCodes, null, practiceSettingCodes,
+                healthCareFacilityCodes, confidentialityCodes, formatCodes, null, AvailabilityStatus.APPROVED);
+            //FindDocumentsQuery fdq = new FindDocumentsQuery(patientId, null, null, null,
+            //    null, null, null, null, AvailabilityStatus.APPROVED);
+            return fdq;
+        }
+
+        public void initConfig()
+        {
+            config.setSTSEndpoint("https://www-test.infomed-vs.ch/dpp-habilitation/X509UserSAMLService");
+            config.setAssertionEndpoint("https://www-test.infomed-vs.ch/dpp-habilitation/AssertionSAMLService");
+            config.setXDSRegistryEndpoint("https://www-test.infomed-vs.ch/dpp-xdsreg");
+            config.setXDSRepositoryEndpoint("https://www-test.infomed-vs.ch/dpp-xdsrep");
+            config.setAuthorId("7601003220445");
+            config.setEmrId("2.16.756.5.30.1.120.14.101");
+            config.setEmrName("Infomed client test Oliver Egger");
+            config.setEmrVersion("1.1");
+            config.setLoginAsApplication(new java.lang.Boolean(true));
+            config.setCertificateSource(XUAConfig.CertificateSource.JKS);
+            config.setX509KeystoreAlias("oliver.egger");
+            config.setX509KeystoreAliasPassword("ahdis");
+            config.setX509KeystorePassword("ahdiskeystore");
+            config.setX509KeystorePath(System.IO.Path.Combine("rsc", "oliver.egger.jks"));
+            config.setLoginWithInstitutionContext(new java.lang.Boolean(true));
+            config.setInstitutionGLN("7601001401310");
+
+            try
+            {
+                java.util.Properties properties = new java.util.Properties();
+
+                properties.put("javax.net.ssl.keyStore", System.IO.Path.Combine("rsc", "empty.jks"));
+                properties.put("javax.net.ssl.keyStorePassword", "changeit");
+                properties.put("javax.net.ssl.trustStore", System.IO.Path.Combine("rsc", "truststoreinfomed.jks"));
+                properties.put("javax.net.ssl.trustStorePassword", "infomed");
+
+                SecurityDomain sd = new SecurityDomain("infomed", properties);
+
+                NodeAuthModuleContext.getContext().getSecurityDomainManager().registerSecurityDomain(sd);
+                NodeAuthModuleContext.getContext().getSecurityDomainManager()
+                        .registerURItoSecurityDomain(new java.net.URI("https://www-test.infomed-vs.ch:443"), "infomed");
+                ConvenienceSecurity.initSecurity();
+            }
+            catch (SecurityDomainException e1)
+            {
+                e1.printStackTrace();
+            }
+            catch (Exception)
+            {
+            }
+        }
+        private ConvenienceCommunication getInfomedCommunication()
+        {
+            AffinityDomain affinityDomain = getInfomedAffinityDomian();
+            ConvenienceCommunication conCom = new ConvenienceCommunication(affinityDomain);
+            return conCom;
+        }
+
+        public AffinityDomain getInfomedAffinityDomian()
+        {
+            Destination infomedReg = null;
+            Destination infomedRep = null;
+            try
+            {
+                infomedReg = new Destination("2.16.756.5.30.1.120.10.1", // new
+                                                                         // URI("http://localhost:8080/dpp-xdsreg"));
+                        new URI("https://www-test.infomed-vs.ch/dpp-xdsreg"));
+                infomedRep = new Destination("2.16.756.5.30.1.120.10.1",
+                        new URI("https://www-test.infomed-vs.ch/dpp-xdsrep"));
+                // new URI("http://localhost:8080/dpp-xdsrep"));
+            }
+            catch (URISyntaxException)
+            {
+            }
+            AffinityDomain affinityDomain = new AffinityDomain(null, infomedReg, infomedRep);
+
+            String endpointPdq = "https://www-test.infomed-vs.ch/PdqInEJB/PDQSupplier_Service/PDQSupplier_PortType";
+            Destination dest = new Destination();
+
+            dest.setSenderApplicationOid("1.2.840.114350.1.13.99998.8735");
+            dest.setSenderFacilityOid("1.2.840.114350.1.13.99998");
+
+            dest.setReceiverApplicationOid("1.3.6.1.4.1.21367.2011.2.2.7872");
+
+            try
+            {
+                dest.setUri(new URI(endpointPdq));
+            }
+            catch (URISyntaxException e)
+            {
+                e.printStackTrace();
+            }
+            affinityDomain.setPdqDestination(dest);
+            return affinityDomain;
         }
     }
 }
